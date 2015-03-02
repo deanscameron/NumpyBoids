682a7cffdc284ae5cac43c4c56ada099eb163149
Cameron Deans <cameron.deans.14@ucl.ac.uk>
2015-03-02 18:54:51 +0000

    Added numpy boid to times for comparison

--- times.py
+++ times.py
@@ -1,5 +1,6 @@
 import timeit
 from badboids import update_badboids, badboids
+from numpyboids import update_boids, boids
 
 def wrapper(func, *args):
     def wrapped():
@@ -7,9 +8,11 @@ def wrapper(func, *args):
     return wrapped
 	
 badboids_original = wrapper(update_badboids, badboids)
+numpyboids = wrapper(update_boids, boids) 
 
 time_badboids = timeit.timeit(badboids_original, number=500)
+time_numpyboids = timeit.timeit(numpyboids, number=500)
 
 print 'Original bad boids time was ' +str(time_badboids)
 print 'My class based bad boids time was ' 
-print 'My Numpy bad boids time is ' 
+print 'My Numpy bad boids time is ' +str(time_numpyboids)
