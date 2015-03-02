e8d36097f57b229c32e1524c5d8b4f31159015ac
Cameron Deans <cameron.deans.14@ucl.ac.uk>
2015-03-02 18:28:35 +0000

    converted fly towards middle to numpy

--- numpyboids.py
+++ numpyboids.py
@@ -12,12 +12,14 @@ boids_y_positions = np.array([random.uniform(300.0,600.0) for x in range(50)])
 boid_x_velocities = np.array([random.uniform(0,10.0) for x in range(50)])
 boid_y_velocities = np.array([random.uniform(-20.0,20.0) for x in range(50)])
 boids=(boids_x_positions,boids_y_positions,boid_x_velocities,boid_y_velocities)
-
+print boids_x_positions
 
 def update_boids(boids):
 	xs,ys,xvs,yvs=boids
 	
 	boid_count = len(xs)
+	match_vel_scale = 0.125
+	nearby_vel_scale = 0.01
 	
 	x_pos_differences = np.add.outer(xs, -xs)
 	y_pos_differences = np.add.outer(ys, -ys)
@@ -25,29 +27,27 @@ def update_boids(boids):
 	boid_distance = np.hypot(x_pos_differences, y_pos_differences)
 	
 	# Fly towards the middle
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+	xvs += nearby_vel_scale*(np.sum(xs)/boid_count - xs)
+	yvs += nearby_vel_scale*(np.sum(ys)/boid_count - ys)
+	
+	
 	# Fly away from nearby boids
 	for i in range(len(xs)):
 		for j in range(len(xs)):
 			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
-				xvs[i]=xvs[i]+(xs[i]-xs[j])
-				yvs[i]=yvs[i]+(ys[i]-ys[j])
+				xvs[i] += (xs[i]-xs[j])
+				yvs[i] += (ys[i]-ys[j])
 	# Try to match speed with nearby boids
 	for i in range(len(xs)):
 		for j in range(len(xs)):
 			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+				xvs[i] += (xvs[j]-xvs[i])*0.125/len(xs)
+				yvs[i] += (yvs[j]-yvs[i])*0.125/len(xs)
 	# Move according to velocities
 	for i in range(len(xs)):
-		xs[i]=xs[i]+xvs[i]
-		ys[i]=ys[i]+yvs[i]
-	print boid_distance
+		xs[i] += xvs[i]
+		ys[i] += yvs[i]
+
 figure=plt.figure()
 axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
 scatter=axes.scatter(boids[0],boids[1])
